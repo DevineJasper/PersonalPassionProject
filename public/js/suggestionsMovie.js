6d632d8bb7e6af6b8c0da1b2a79a23209160cb4a
DevineJasper <jasper.vermeesch@student.howest.be>
2019-12-10 02:06:38 +0100

    frontend optimalisation + backend optimalisation

--- public/js/suggestionsMovie.js
+++ public/js/suggestionsMovie.js
@@ -1,17 +1,10 @@
 console.log('movieDb.js');
 let movies = [];
-let currentSuggestions = [];
-let amountSuggestions = document.querySelector('.suggestionsLength').innerHTML;
-let userSuggestionsDb = false;
 let newSuggestions = [];
 let removedSuggestions = [];
-let originalSuggestions = [];
 
 const $ul = document.querySelector('.list');
 const $suggestionsUl = document.querySelector('.currentSuggestions');
-
-const url = document.querySelector('.url').innerHTML;
-const user = document.querySelector('.psid').innerHTML;
 
 //messenger extensions js SDK
 function loadMessengerSDK(d, s, id) {
@@ -52,37 +45,6 @@ window.fbAsyncInit = () => {
 };
 
 //AJAX API
-const getUserSuggestions = () => {
-	get(`${url}/suggestions/movies/${user}`).then(r => {
-		if (r.length == 0) {
-			document.querySelector('.zoek').classList.remove('hidden');
-			$ul.classList.remove('hidden');
-		} else {
-			userSuggestionsDb = true;
-			r.forEach(object => {
-				originalSuggestions.push(object.movieId);
-				addUserSuggestions(object.movieId);
-			});
-		}
-	});
-};
-
-const addUserSuggestions = async movieId => {
-	let movieObject = await fetch(
-		`https://api.themoviedb.org/3/movie/${movieId}?api_key=a108ea578de94e9156c38073bbd89613&language=en-EN`
-	)
-		.then(r => r.json())
-		.then(data => {
-			return data;
-		});
-	currentSuggestions.push(movieObject);
-	renderSuggestions();
-};
-
-const fetchAll = url => {
-	get(url).then(data => addMovies(data));
-};
-
 const addMovies = data => {
 	movies = [];
 	const results = data.results;
@@ -94,153 +56,205 @@ const addMovies = data => {
 
 const renderList = () => {
 	while ($ul.firstChild) $ul.removeChild($ul.firstChild);
-	movies.forEach(row => {
-		console.log(row.poster_path);
+	if (movies.length == 0) {
 		const $newLi = document.createElement('li');
-		const $newArticle = document.createElement('article');
-		$newArticle.classList.add('movieGrid');
-		const $newHeading = document.createElement('h3');
-		$newHeading.classList.add('movieTitle');
-		const $newParagraph = document.createElement('p');
-		$newParagraph.classList.add('movieOverview');
-		const $newImg = document.createElement('img');
-		$newImg.classList.add('moviePoster');
-		const $newAnchor = document.createElement('a');
-		$newAnchor.classList.add('info');
-		const $newAdd = document.createElement('p');
-		$newAdd.classList.add('add');
-		const $newButtons = document.createElement('div');
-		$newButtons.classList.add('buttons');
-		$newButtons.appendChild($newAdd);
-		$newButtons.appendChild($newAnchor);
-
-		const title = row.title;
-		const overview = row.overview;
-		if (row.poster_path !== null) {
-			const poster = row.poster_path;
-			$newImg.src = `https://image.tmdb.org/t/p/w185/${poster}`;
-		} else {
-			$newImg.src = `/assets/images/backdrop_poster.png`;
-		}
-		const id = row.id;
-
-		$newAdd.innerHTML = 'ADD';
-		$newAdd.dataset.movieId = row.id;
-		$newAdd.addEventListener('click', handleAdd);
-		$newAnchor.setAttribute('target', '_blank');
-		$newAnchor.href = `https://themoviedb.org/movie/${id}?language=en-EN`;
-		$newAnchor.innerHTML = 'meer info';
-		$newArticle.appendChild($newImg);
-		$newHeading.innerHTML = title;
-		$newArticle.appendChild($newHeading);
-		$newParagraph.innerHTML = overview;
-		$newArticle.appendChild($newParagraph);
-		$newArticle.appendChild($newButtons);
-		$newLi.appendChild($newArticle);
+		$newLi.classList.add('instruction');
+		$newLi.innerHTML = 'Geen films gevonden...';
 		$ul.appendChild($newLi);
-	});
+	} else {
+		movies.forEach(row => {
+			console.log(row.poster_path);
+			const $newLi = document.createElement('li');
+			const $newArticle = document.createElement('article');
+			$newArticle.classList.add('movieGrid');
+			const $newHeading = document.createElement('h2');
+			$newHeading.classList.add('movieTitle');
+			const $newParagraph = document.createElement('p');
+			$newParagraph.classList.add('movieOverview');
+			const $newImg = document.createElement('img');
+			$newImg.classList.add('moviePoster');
+			const $newAnchor = document.createElement('a');
+			$newAnchor.classList.add('info', 'button');
+			const $newAdd = document.createElement('p');
+			$newAdd.classList.add('add');
+			const $newButtons = document.createElement('div');
+			$newButtons.classList.add('adminMovieButtons');
+			$newButtons.appendChild($newAdd);
+			$newButtons.appendChild($newAnchor);
+			$newSpan = document.createElement('span');
+			$newSpan.classList.add('ext-link');
+
+			const title = row.title;
+			const overview = row.overview;
+			if (row.poster_path !== null) {
+				const poster = row.poster_path;
+				$newImg.src = `https://image.tmdb.org/t/p/w185/${poster}`;
+			} else {
+				$newImg.src = `/assets/images/backdrop_poster.png`;
+			}
+			const id = row.id;
+
+			let inCurrentArray = false;
+			currentSuggestions.forEach(suggestion => {
+				if (suggestion.id === row.id) {
+					inCurrentArray = true;
+				}
+			});
+			if (inCurrentArray == false) {
+				$newAdd.innerHTML = 'voeg toe';
+				$newAdd.classList.add('button');
+			} else {
+				$newAdd.innerHTML = 'toegevoegd';
+				$newAdd.classList.add('disabledButton');
+			}
+			$newAdd.addEventListener('click', () => handleAdd(row));
+			$newAnchor.setAttribute('target', '_blank');
+			$newAnchor.href = `https://themoviedb.org/movie/${id}?language=en-EN`;
+			$newAnchor.innerHTML = 'meer info';
+			$newAnchor.appendChild($newSpan);
+			$newArticle.appendChild($newImg);
+			$newHeading.innerHTML = title;
+			$newArticle.appendChild($newHeading);
+			$newParagraph.innerHTML = overview;
+			$newArticle.appendChild($newParagraph);
+			$newArticle.appendChild($newButtons);
+			$newLi.appendChild($newArticle);
+			$ul.appendChild($newLi);
+		});
+	}
 };
 
-const handleAdd = async e => {
-	const movieId = e.currentTarget.dataset.movieId;
-	let movieObject = await fetch(
-		`https://api.themoviedb.org/3/movie/${movieId}?api_key=a108ea578de94e9156c38073bbd89613&language=nl-NL&`
-	)
-		.then(r => r.json())
-		.then(data => {
-			return data;
-		});
-	if (amountSuggestions < 3 && amountSuggestions > 0) {
-		console.log('minder dan 3 suggesties');
-		let objectInArray = false;
-		currentSuggestions.forEach(movie => {
-			if (movie.id === movieObject.id) {
-				objectInArray = true;
-			}
-		});
-		if (objectInArray === false) {
-			currentSuggestions.push(movieObject);
-			newSuggestions.push(movieObject);
-			console.log(currentSuggestions);
+const handleAdd = movie => {
+	console.log(movie);
+	switch (true) {
+		case amountSuggestions == 0:
+			console.log('nog geen suggestions');
+			currentSuggestions.push(movie);
+			newSuggestions.push(movie);
 			renderSuggestions();
-		}
-	} else if (amountSuggestions == 0) {
-		currentSuggestions.push(movieObject);
-		newSuggestions.push(movieObject);
-		console.log(currentSuggestions);
-		renderSuggestions();
-	} else {
-		document.querySelector('.amount').classList.remove('redAnimation');
-		document.querySelector('.amount').classList.add('redAnimation');
-		setTimeout(
-			() => document.querySelector('.amount').classList.remove('redAnimation'),
-			200
-		);
+			renderList(movies);
+			break;
+		case amountSuggestions > 0 && amountSuggestions < 3:
+			let isInArray = false;
+			currentSuggestions.forEach(suggestion => {
+				if (suggestion.id === movie.id) {
+					isInArray = true;
+				}
+			});
+			if (isInArray === false) {
+				console.log('welkom');
+				newSuggestions.push(movie);
+				currentSuggestions.push(movie);
+				renderSuggestions();
+				renderList(movies);
+			}
+			break;
+		case amountSuggestions >= 3:
+			console.log('meer dan 3');
+			document.querySelector('.amount').classList.add('redAnimation');
+			setTimeout(
+				() =>
+					document.querySelector('.amount').classList.remove('redAnimation'),
+				1000
+			);
+			break;
 	}
 };
 
 const renderSuggestions = () => {
+	let $renderAmount = document.querySelector('.amount');
 	while ($suggestionsUl.firstChild)
 		$suggestionsUl.removeChild($suggestionsUl.firstChild);
 	amountSuggestions = 0;
-	document.querySelector('.amount').innerHTML = amountSuggestions;
+	$renderAmount.innerHTML = amountSuggestions;
 	currentSuggestions.forEach(suggestion => {
 		amountSuggestions++;
-		document.querySelector('.amount').innerHTML = amountSuggestions;
-		const $newCont = document.createElement('div');
-		const $newHeader = document.createElement('h3');
-		const $newThumb = document.createElement('img');
-		const $newDiv = document.createElement('div');
+		$renderAmount.innerHTML = amountSuggestions;
 
-		$newDiv.innerHTML = 'X';
-		$newHeader.innerHTML = suggestion.title;
-		$newCont.appendChild($newThumb);
-		$newCont.appendChild($newHeader);
-		$newCont.appendChild($newDiv);
-		$suggestionsUl.appendChild($newCont);
-		$newThumb.src = `https://image.tmdb.org/t/p/w185/${suggestion.poster_path}`;
-		$newDiv.addEventListener('click', () => {
+		const $userMovieItem = document.createElement('li');
+		$userMovieItem.classList.add('userMovieItem');
+
+		const $userMovieGrid = document.createElement('article');
+		$userMovieGrid.classList.add('userMovieGrid');
+
+		const $userMovieHeader = document.createElement('header');
+		$userMovieHeader.classList.add('userMovieHeader');
+
+		const $userMovieTitle = document.createElement('h2');
+		$userMovieTitle.classList.add('userMovieTitle');
+		$userMovieTitle.innerHTML = suggestion.title;
+
+		$userMovieHeader.appendChild($userMovieTitle);
+		$userMovieGrid.appendChild($userMovieHeader);
+
+		const $userMoviePoster = document.createElement('img');
+		$userMoviePoster.classList.add('userMoviePoster');
+		if (suggestion.poster_path !== null) {
+			$userMoviePoster.src = `https://image.tmdb.org/t/p/w185/${suggestion.poster_path}`;
+		} else {
+			$userMoviePoster.src = `/assets/images/backdrop_poster.png`;
+		}
+
+		$userMovieGrid.appendChild($userMoviePoster);
+
+		const $userMovieDelete = document.createElement('div');
+		$userMovieDelete.classList.add('userMovieDelete');
+
+		const $span = document.createElement('span');
+		$span.innerHTML = 'verwijder';
+		$userMovieDelete.appendChild($span);
+
+		$userMovieDelete.addEventListener('click', () => {
 			removeSuggestion(suggestion);
 		});
+
+		$userMovieGrid.appendChild($userMovieDelete);
+
+		$userMovieItem.appendChild($userMovieGrid);
+
+		$suggestionsUl.appendChild($userMovieItem);
 	});
 };
 
-const removeSuggestion = suggestion => {
-	removedSuggestions.push(suggestion.id);
-	console.log(removedSuggestions);
-	newSuggestions.forEach(movie => {
-		if (movie.id === suggestion.id) {
-			newSuggestions.splice(newSuggestions.indexOf(movie), 1);
-			removedSuggestions.splice(removedSuggestions.indexOf(movie), 1);
+const removeSuggestion = removed => {
+	removedSuggestions.push(removed);
+	newSuggestions.forEach(newSuggestion => {
+		if (newSuggestion.id === removed.id) {
+			newSuggestions.splice(newSuggestions.indexOf(newSuggestion), 1);
+			removedSuggestions.splice(removedSuggestions.indexOf(removed), 1);
 		}
 	});
-	currentSuggestions.forEach(movie => {
-		if (movie.id === suggestion.id) {
-			currentSuggestions.splice(currentSuggestions.indexOf(movie), 1);
+	currentSuggestions.forEach(suggestion => {
+		if (suggestion.id === removed.id) {
+			currentSuggestions.splice(currentSuggestions.indexOf(suggestion), 1);
 			renderSuggestions();
+			renderList(movies);
 		}
 	});
 };
 
-const handleZoek = e => {
+const handleZoek = async e => {
 	let value = e.currentTarget.value;
-	console.log(value);
 	if (value !== '') {
-		fetchAll(
+		await get(
 			`https://api.themoviedb.org/3/search/movie?api_key=a108ea578de94e9156c38073bbd89613&language=en-EN&query=${value}&page=1&include_adult=false`
-		);
+		).then(data => {
+			addMovies(data);
+		});
 	} else {
 		while ($ul.firstChild) $ul.removeChild($ul.firstChild);
 		const $newLi = document.createElement('li');
-		$newLi.innerHTML = 'Zoek naar een film!';
 		$newLi.classList.add('instruction');
 		$ul.appendChild($newLi);
+		$newLi.innerHTML = 'Zoek naar een film...';
 	}
 };
 
 const handleSuggestionsClick = e => {
 	const suggestionsWindow = document.querySelector('.suggestionsWindow');
 	suggestionsWindow.classList.toggle('fullScreen');
+	const suggestionsArrow = document.querySelector('.suggestionsArrow');
+	suggestionsArrow.classList.toggle('down');
 	document.querySelector('html').classList.toggle('noScrollY');
 	document.querySelector('.searchContainer').classList.toggle('transparant');
 };
@@ -288,8 +302,10 @@ const lowerBottom = () => {
 };
 
 const init = () => {
+	if (currentSuggestions.length > 0 && userSuggestionsDb == true) {
+		renderSuggestions();
+	}
 	loadMessengerSDK(document, 'script', 'Messenger');
-	// getUserSuggestions();
 	document.querySelector('.submit').addEventListener('click', handleSubmit);
 	if (
 		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
@@ -301,7 +317,7 @@ const init = () => {
 	}
 	document.querySelector('.zoek').addEventListener('input', handleZoek);
 	document
-		.querySelector('.suggestionsBtn')
+		.querySelector('.suggestionsBtnCont')
 		.addEventListener('click', handleSuggestionsClick);
 };
 init();
